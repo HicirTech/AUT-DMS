afbf495a736d32a0a00101acb4cc3f4b266c49e3
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2019-02-28 17:03:35 +1300

    ASK a ball app is finished

---
+++ src/lab1/Test.java
@@ -0,0 +1,14 @@
+package lab1;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+	}
+
+}
