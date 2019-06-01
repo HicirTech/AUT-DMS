0bb50b16a656edcd6884d8380dbbd587ed3820a5
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-01 20:04:36 +1200

    test on Queue

--- TestSTS/JMS_V2/src/main/java/com/testing/JMSV2/App.java
+++ TestSTS/JMS_V2/src/main/java/com/testing/JMSV2/App.java
@@ -2,8 +2,7 @@ package com.testing.JMSV2;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
-import javax.jms.JMSContext;
-import javax.jms.Queue;
+import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -13,7 +12,7 @@ import javax.naming.NamingException;
  */
 public class App 
 {
-    public static void main( String[] args ) throws NamingException {
+    public static void main( String[] args ) throws NamingException, JMSException {
         InitialContext initialContext = new InitialContext();
         Queue queue  = (Queue) initialContext.lookup("queue/myQueue");
 
@@ -27,5 +26,32 @@ public class App
             String message = jmsContext.createConsumer(queue).receiveBody(String.class);
             System.out.println("[GET] "+message);
         }
+        
+        //reply
+        try(ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        		JMSContext jmsContext = connectionFactory.createContext() ){
+            Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+            Queue replyQueue = (Queue)initialContext.lookup("queue/replyQueue");
+
+            JMSProducer realProducer = jmsContext.createProducer();
+            TextMessage message = jmsContext.createTextMessage("<- [PRODUCER]: here is the message");
+            message.setJMSReplyTo(replyQueue);
+            realProducer.send(requestQueue,message); //message is out
+
+            JMSConsumer consumer  = jmsContext.createConsumer(requestQueue);
+            TextMessage textMessage = (TextMessage) consumer.receive();
+            System.out.println("[CONSUMER HAS THE MESSAGE]: "+textMessage.getText());
+
+            JMSProducer replyProducer = jmsContext.createProducer();
+            replyProducer.send(message.getJMSReplyTo()," <-[AFTER CONSUMER GET THE MESSAGE]: Reply back to where the message should go" );
+
+            JMSConsumer replyConsummer = jmsContext.createConsumer(replyQueue);
+            System.out.println("[REPLY CONSUMER HAS MESSAGE]:"+replyConsummer.receiveBody(String.class));
+
+
+        	
+        }
+        
+        
     }
 }
